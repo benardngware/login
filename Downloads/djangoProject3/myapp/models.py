cdf46d4efa6195aad15dd93e2423865fe9538377
benardngware <benardngware77@gmail.com>
2023-04-24 16:28:35 +0300

    hey

--- Downloads/djangoProject3/myapp/models.py
+++ Downloads/djangoProject3/myapp/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Member(models.Model):
-    firstname=models.CharField(max_length=50)
-    lastname= models.CharField(max_length=50)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
     username = models.CharField(max_length=50)
-    password=models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
 
     def __str__(self):
-        self.firstname+' '+self.lastname
+         self.firstname + ' ' + self.lastname
